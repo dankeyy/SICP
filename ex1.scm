902173433cb45db1d98a303ac5396a6a69ef5e9b
dankeyy <dankeyy@protonmail.com>
2022-05-17 17:10:54 +0300

    fixed 1.11 and done with 1.12

--- ex1.scm
+++ ex1.scm
@@ -185,10 +185,17 @@
   (define (iter a b c n)
     (if (< n 3)
       a
-      (iter
-          (+ a (* 2 b) (* 3 c))
-          a
-          b
-          (- n 1))))
-
+      (iter (+ a (* 2 b) (* 3 c))
+            a
+            b
+            (- n 1))))
   (iter 2 1 0 n))
+
+
+;; ex 1.12
+(define (pascals-triangle r c)
+  (if (or (= c 1) (= r c))
+      1
+      (+
+        (pascals-triangle (- r 1) (- c 1))
+        (pascals-triangle (- r 1)  c))))
